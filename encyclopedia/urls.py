180b5e6b981dbfcf575aa1b7bb9fd0e90c4f5853
Willie1416 <whall4441@gmail.com>
2024-05-22 15:20:54 -0500

    Final program

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
     path("search/", views.search, name="search"),
-    path("wiki/", views.randomize, name="randomize"),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("edit/", views.edit, name="edit"),
+    path("save_edit", views.save_edit, name="save_edit"),
+    path("wiki/", views.randomize, name="randomize")
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -19,14 +19,14 @@ def wiki(request, title):
     content = util.get_entry(title)
     
     if content is None:
-        return render(request, "encyclopedia/error.html", {"title": title})
+        return render(request, "encyclopedia/error.html", {"title": title, "message": 'page does not exist'})
     else:
         return render(request, "encyclopedia/wiki.html", 
             {"title": title, "content": markdown(content)})
     
 def search(request):
     if request.method == 'POST':
-        query = request.POST.get('q', None) # Gets search query
+        query = request.POST.get('q', None) 
         if query:
             content = util.get_entry(query)
             if content:
@@ -35,18 +35,58 @@ def search(request):
             else:
                 suggestions = [entry for entry in entries if query.lower() in entry.lower()]
                 if suggestions:
-                    return render(request, "encyclopedia/suggestions.html", {"title": query, "suggestions": suggestions})
+                    return render(request, "encyclopedia/search.html", {"title": query, "suggestions": suggestions})
                 else:
-                    return render(request, "encyclopedia/error.html", {"title": query})
+                    return render(request, "encyclopedia/error.html", {'title': query, "message": 'page does not exist'})
 
 
 def create(request):
-    return render(request, "encyclopedia/create.html")
+    if request.method == "GET":
+        return render(request, "encyclopedia/create.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render(request, "encyclopedia/error.html", {
+                'title': title, "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(title, content)
+            return render(request, "encyclopedia/wiki.html", {
+                "title": title,
+                "content": markdown(content)
+            })
+
+
+def edit(request):
+    if request.method == "POST":
+        title = request.POST['entry_title']
+        content = util.get_entry(title)
+        
+        return render(request, "encyclopedia/edit.html",{
+            "title": title,
+            "content": content
+        })
+    
+def save_edit(request):
+    if request.method =="POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/wiki.html", {
+            "title": title,
+            "content": markdown(content)
+        })
+
 
 def randomize(request):
     entry = random.choice(entries)
     content = util.get_entry(entry)
-    return render(request, "encyclopedia/wiki.html", 
+    try:
+        return render(request, "encyclopedia/wiki.html", 
                   {"title": entry, "content": markdown(content)})
+    except FileNotFoundError:
+        return None
 
 
